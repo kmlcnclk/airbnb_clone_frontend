b265ea62e94e710a1d00c8681b037168a8189121
Kamilcan Çelik <kamilcancelik5757@gmail.com>
2022-08-29 15:15:04 +0300

    Update Icons.tsx

--- src/Icons.tsx
+++ src/Icons.tsx
@@ -94,7 +94,7 @@ export const MenuIcon = ({
       className={className}
       stroke={color}
     >
-      <g fill="none" fill-rule="nonzero">
+      <g fill="none" fillRule="nonzero">
         <path d="m2 16h28"></path>
         <path d="m2 24h28"></path>
         <path d="m2 8h28"></path>
