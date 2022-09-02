b991b845e034fb24378121d260ef8df8d2c0c799
Kamilcan Çelik <kamilcancelik5757@gmail.com>
2022-09-02 14:26:31 +0300

    adding site logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import Header from '../components/header';
 
 const Home: NextPage = () => {
   return (
     <div>
+      <Head>
+        <title>Vacation Homes & Condo Rentals - Airbnb - Airbnb</title>
+        <meta
+          name="description"
+          content="Find vacation rentals, cabins, beach houses, unique homes and experiences around the world - all made possible by hosts on Airbnb."
+        />
+        <link rel="icon" href="/airbnb_logo.png" />
+      </Head>
       <Header />
     </div>
   );
